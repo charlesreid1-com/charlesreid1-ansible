08efa32f4c14b9077d7316254c6e29eb8dcfe987
C Reid <charlesreid1@Gmail.com>
2019-02-19 21:32:56 -0800

    remove all the dotfiles and use the debian dotfiles repo instead

--- roles/dotfiles/files/krash_scripts/git_pull_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git -C /www/charlesreid1.com \
-    --git-dir=git.data --work-tree=htdocs/data \
-    pull origin master
-
--- roles/dotfiles/files/krash_scripts/git_pull_www.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git -C /www/charlesreid1.com \
-    --git-dir=git --work-tree=htdocs \
-    pull origin gh-pages
-
